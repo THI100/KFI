8be48d8236c915776973de03438acd16e7739f25
THI100 <thiagocas100@gmail.com>
2026-07-30 21:52:49 -0300

    Finalized init command, working fine with a minor inconveniency at vault.json, may require the user to do manual trimming.

--- runner/src/management/init.rs
+++ runner/src/management/init.rs
@@ -5,7 +5,8 @@ use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 type Errors = Box<dyn Error>;
 
@@ -35,38 +36,110 @@ struct SafeSt {
 }
 
 pub fn run(args: models::InitArgs) -> Result<(), Errors> {
-    return Err(format!(
-        "Work in progress, but this is the command and arguments: Init: {args:#?}"
-    )
-    .into());
+    let location = args.location;
+    let vault_path = location.join(".vault");
+    let bypass_path = location.join(".vaultbypass");
 
-    // Initialization
-    let pathv: PathBuf = args.location.join(PathBuf::from("/.vault"));
-    let pathf: PathBuf = args.location.join(PathBuf::from("/.vaultbypass"));
+    if location.exists() && !location.is_dir() {
+        return Err(format!("Init location is not a directory: {}", location.display()).into());
+    }
+    fs::create_dir_all(&location)?;
 
-    // Checks
-    if pathf.is_file() {
-        println!("Warning: .vaultbypass file is existent");
+    if vault_path.exists() {
+        return Err(format!("Vault already exists: {}", vault_path.display()).into());
+    }
+
+    let bypass_created = if bypass_path.exists() {
+        if !bypass_path.is_file() {
+            return Err(format!("Bypass path is not a file: {}", bypass_path.display()).into());
+        }
+        false
     } else {
-        File::create_new(pathf)?;
+        File::create_new(&bypass_path)?;
+        true
+    };
+
+    // Create the complete initial vault layout.
+    for directory in [
+        "branches/trunk/saves",
+        "branches/trunk/refs",
+        "objects/blobs",
+        "objects/trees",
+        "objects/manifests",
+        "objects/metadata",
+        "info/sessions",
+        "temp/unaudited_saves",
+    ] {
+        fs::create_dir_all(vault_path.join(directory))?;
     }
 
-    if pathv.is_dir() {
-        return Err(".vault folder is existent".into());
+    let created = SystemTime::now()
+        .duration_since(UNIX_EPOCH)?
+        .as_secs()
+        .to_string();
+    let config = Config {
+        vault: VaultSt {
+            name: args.vault.clone(),
+            created: created.clone(),
+            version: 1,
+        },
+        defaults: DefSt {
+            branch: "trunk".into(),
+        },
+        safety: SafeSt {
+            create_bypass: true,
+        },
+    };
+
+    write_new(&vault_path.join("REFER"), b"branches/trunk/HEAD\n")?;
+    write_new(
+        &vault_path.join("config.toml"),
+        toml::to_string_pretty(&config)?.as_bytes(),
+    )?;
+    write_new(&vault_path.join("branches/trunk/HEAD"), b"Genesis\n")?;
+
+    let vault_info = format!(
+        "{{\n  \"id\": {},\n  \"created\": {},\n  \"last_save\": null,\n  \"format\": 1\n}}\n",
+        json_string(&args.vault),
+        json_string(&created),
+    );
+    write_new(&vault_path.join("info/vault.json"), vault_info.as_bytes())?;
+    write_new(
+        &vault_path.join("info/statistics.json"),
+        b"{\n  \"saves\": 0,\n  \"branches\": 1,\n  \"blobs\": 0,\n  \"encrypted\": false,\n  \"size\": 0\n}\n",
+    )?;
+
+    // sled creates and initializes its database at this path.
+    let db = sled::open(vault_path.join("index"))?;
+    db.flush()?;
+
+    if bypass_created {
+        println!(
+            "Initialized vault at {} and created .vaultbypass",
+            location.display()
+        );
+    } else {
+        println!(
+            "Initialized vault at {} (.vaultbypass already existed)",
+            location.display()
+        );
     }
 
-    // Create folders
-    fs::create_dir_all(pathv)?;
-    fs::create_dir(pathv.join(PathBuf::from("/branches")))?;
-    fs::create_dir_all(pathv.join(PathBuf::from("/trunk/saves")))?;
-    fs::create_dir_all(pathv.join(PathBuf::from("/trunk/refs")))?;
-    fs::create_dir(pathv.join(PathBuf::from("/objects")))?;
+    Ok(())
+}
 
-    // Create files
-    let mut ref = File::create_new(pathv.join(PathBuf::from("REFER")))?; // Actual branch, actual save
-    let mut conf = File::create_new(pathv.join(PathBuf::from("config.toml")))?; // Config file
-    let db: sled::Db = sled::open(pathv.join(PathBuf::from("index"))).unwrap(); // Index File
+fn write_new(path: &Path, data: &[u8]) -> Result<(), Errors> {
+    let mut file = File::create_new(path)?;
+    file.write_all(data)?;
+    Ok(())
+}
 
-    // Edit and add data to files
-    ref.write_all("./branches/trunk/HEAD".as_bytes())?;
+fn json_string(value: &str) -> String {
+    let escaped = value
+        .replace('\\', "\\\\")
+        .replace('\"', "\\\"")
+        .replace('\n', "\\n")
+        .replace('\r', "\\r")
+        .replace('\t', "\\t");
+    format!("\\\"{escaped}\\\"")
 }
