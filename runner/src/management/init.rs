ff9e002fbd59159ae1626e8ab889bf34aa8a9472
THI100 <thiagocas100@gmail.com>
2026-07-31 12:51:59 -0300

    Small planning for open command, may have some variations in the future.

--- runner/src/management/init.rs
+++ runner/src/management/init.rs
@@ -1,6 +1,7 @@
 use cli::models;
 use serde::Serialize;
 use sled;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
@@ -33,6 +34,14 @@ struct DefSt {
 #[derive(Debug, Serialize)]
 struct SafeSt {
     create_bypass: bool,
+}
+
+// Structs for store.toml
+
+#[derive(Debug, Serialize)]
+struct Store {
+    active: String,
+    vaults: HashMap<String, String>,
 }
 
 pub fn run(args: models::InitArgs) -> Result<(), Errors> {
