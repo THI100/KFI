031ab7207a38820c8f4fdfd4cc91da8da4c9ea94
THI100 <thiagocas100@gmail.com>
2026-08-05 22:27:59 -0300

    Added cc and preparing a new hashing function for multiple files

--- helper/src/crypto.rs
+++ helper/src/crypto.rs
@@ -10,3 +10,11 @@ pub fn encode_hash(algo: &str, input: Vec<u8>, outsize: Option<u32>) -> Result<S
 }
 
 pub fn encode_encryption() -> Result<String, Errors> {}
+
+pub fn encode_stream_hash(algo: &str, input: Vec<u8>, outsize: Option<u32>) -> Result<String, Errors> {
+    match algo {
+        "Blake3" => {}
+        "Kaurea" => {}
+        "Sha3" => {}
+    }
+}
