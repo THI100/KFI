15aaaecabe7dfec58621b6c8fe9b9bd20e5b441b
THI100 <thiagocas100@gmail.com>
2026-08-21 19:37:51 -0300

    Making the part 2 of the function.

--- runner/src/tracking/add.rs
+++ runner/src/tracking/add.rs
@@ -1,6 +1,8 @@
 use cli::models;
 use helper::read_store;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 use walkdir::WalkDir;
 
 type Errors = Box<dyn Error>;
@@ -10,12 +12,25 @@ pub fn run(args: models::AddArgs) -> Result<(), Errors> {
     //     format!("Work in progress, but this is the command and arguments: Add: {args:#?}").into(),
     // );
 
-    let alive = read_store();
+    let alive = read_store()?;
     let paths = Vec::new();
 
     if args.all == true {
         for path in WalkDir::new(alive).into_iter().filter_map(|e| e.ok()) {
-            paths.push(path);
+            paths.push(alive.join(path));
+        }
+
+        let bypass = File::open(alive.join(".vaultbypass"))?;
+        let reader = BufReader::new(bypass);
+        let mut tmp = String::new();
+        while reader.read_line(&mut tmp)? > 0 {
+            // remove an path from the paths, considering:
+            // that starts with * is all files that has the suffix: .rs, .html, .py
+            // that starts with # will be ignored
+            // that starts with / is a folder all of its contents will be excluded
+            // that starts with " " or . is a file
+
+            tmp.clear();
         }
     } else {
         if !args.files.map_or(true, |v| v.is_empty()) {
